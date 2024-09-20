48f907ef63cfd9ad29d66196e884e823508f5ece
LucasArd <luqasaranda@outlook.com>
2024-09-20 09:39:19 -0300

    Posnet structure

---
+++ src/main/java/com/proyectoposnet/posnet321/Posnet321.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyectoposnet.posnet321;
+
+/**
+ *
+ * @author luqas
+ */
+public class Posnet321 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
