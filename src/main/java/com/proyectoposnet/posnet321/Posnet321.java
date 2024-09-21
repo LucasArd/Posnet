1ab9d63282ed3d72882d551b81834c46d2f14988
LucasArd <luqasaranda@outlook.com>
2024-09-21 12:10:56 -0300

    Posnet structure modificaciones

--- src/main/java/com/proyectoposnet/posnet321/Posnet321.java
+++ src/main/java/com/proyectoposnet/posnet321/Posnet321.java
@@ -11,6 +11,12 @@ package com.proyectoposnet.posnet321;
 public class Posnet321 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cliente cliente1 = new Cliente("39466587","Lucas", "Aranda", "12555666","lucas@hotmail.com");
+        TarjetaCredito tarjeta1 = new TarjetaCredito(EntidadFinanciera.VISA, "BBVA", "4545555666111",cliente1,30000);
+        
+        Posnet posnet1 = new Posnet();
+        
+        System.out.print(posnet1.efectuarPago(tarjeta1, 20000, 2));
+        
     }
-}
+} 
